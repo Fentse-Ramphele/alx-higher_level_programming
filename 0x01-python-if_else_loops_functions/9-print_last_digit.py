8095d45895d04965c1cb4b8e635732234698ff5f
Fentse-Ramphele <framphele18@gmail.com>
2024-01-04 15:58:13 -0800

    There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-def uppercase(str):
-    new_str = ""
-    for i in str:
-        if i >= 'a' and i <= 'z':
-            new_str = new_str + chr((ord(i) - 32))
-        else:
-            new_str = new_str + i
+def print_last_digit(number):
+    if number < 0:
+        new_number = number * -1
+        last_digit = (-1) * (new_number % 10)
+        last_digit = last_digit * -1
+    else:
+        last_digit = number % 10
 
-    print("{}".format(new_str))
+    print("{:d}".format(last_digit), end='')
+    return last_digit
